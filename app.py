25b0c6590f56d0287ac86a46ff6020c14b9c9090
KolluriChandrakala <163409907+KolluriChandrakala@users.noreply.github.com>
2025-07-17 14:57:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def firstApp():
-    return "Hello world to github"
+    return "Hello world to github to greetings"
+    
 app.run(debug = True)
 
 
